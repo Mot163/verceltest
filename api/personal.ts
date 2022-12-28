cd174c75ea1da554f2a5be8016e7d79c2e7a71b5
Mot163 <modcloud@163.com>
2022-12-28 11:49:29 +0800

    init

--- api/personal.ts
+++ api/personal.ts
@@ -1,6 +1,23 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+// import WebApplication from '../dispatcher/WebApplication';
 
-export default function (req: VercelRequest, res: VercelResponse) {
+export default (req: VercelRequest, res: VercelResponse) => {
+    // WebApplication.run().then((service) => {
+    //     const response = service({
+    //         url: 'http://127.0.0.1/user/123',
+    //         method: 'POST',
+    //         headers: {
+    //             'content-type': 'application/json',
+    //             'x-client-ip': '127.0.0.1',
+    //             host: '127.0.0.1'
+    //         },
+    //         body: JSON.stringify({
+    //             username: 'bingjin',
+    //             password: '123456'
+    //         })
+    //     });
+    //     console.log(response);
+    // });
     const { name = 'World' } = req.query;
-    res.send(`Hello ${name}!`);
-}
+    res.send(req);
+};
