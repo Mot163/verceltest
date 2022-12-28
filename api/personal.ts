04e55ce57a5118067bd310778e3c675210bfa5ae
Mot163 <modcloud@163.com>
2022-12-28 15:25:16 +0800

    init

--- api/personal.ts
+++ api/personal.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default async (req: VercelRequest, res: VercelResponse) => {
-    res.send('' + MONGODB_URI);
+    res.send('' + process.env.MONGODB_URI);
 };
