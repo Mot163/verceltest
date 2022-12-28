966c788c03b315a551ef285702106180d623ca65
Mot163 <modcloud@163.com>
2022-12-28 12:51:14 +0800

    init

--- api/personal.ts
+++ api/personal.ts
@@ -2,5 +2,5 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function (req: VercelRequest, res: VercelResponse) {
     const { name = 'World' } = req.query;
-    res.send(`Hello ${name}`);
+    res.send(`Hello ${req.url}`);
 };
